fef48564811528853fd7d174e45bd533326400cb
Maciej <metro1568@gmail.com>
2025-04-29 13:20:56 +0200

    Fixed nr 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,9 @@ const pageThumb = document.getElementById('thumbs');
 pageThumb.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const thumbImageLink = e.target.closest('a').href;
+  const thumbImageLink = e.target.closest('a')
+    ? e.target.closest('a').href
+    : null;
 
   if (thumbImageLink) {
     bigImage.src = thumbImageLink;
