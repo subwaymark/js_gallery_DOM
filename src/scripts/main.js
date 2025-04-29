2adeca488db96a7a8b3a1f4b2c63b60e23fc34da
Maciej <metro1568@gmail.com>
2025-04-29 13:15:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bigImage = document.getElementById('largeImg');
+const pageThumb = document.getElementById('thumbs');
+
+pageThumb.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const thumbImageLink = e.target.closest('a').href;
+
+  if (thumbImageLink) {
+    bigImage.src = thumbImageLink;
+  }
+});
